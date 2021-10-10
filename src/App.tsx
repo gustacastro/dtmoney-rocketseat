20dea4f99295f603d6f26099880b08dc713f8a74
Gustavo Castro <gustavo@lucjedi.com>
2021-10-09 23:56:16 -0300

    Create layout / modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal}/>
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+      
+      <GlobalStyle />
+    </>
   );
 }
